c60513873c486918d510bea259838b0d048e9a50
Ben Hessel <hesselfitness@gmail.com>
2023-09-07 14:26:58 -0400

    default value set in dropdown

--- src/components/CategoryDropdown.tsx
+++ src/components/CategoryDropdown.tsx
@@ -10,7 +10,9 @@ export default function CategoryDropdown(props) {
       <Listbox value={selectedCategory} onChange={setSelectedCategory}>
         <div className="relative py-3 w-1/4">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">{selectedCategory}</span>
+            <span className="block truncate">
+              {selectedCategory || "Select a Category"}
+            </span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
                 className="h-5 w-5 text-gray-400"
@@ -35,16 +37,16 @@ export default function CategoryDropdown(props) {
                   }
                   value={eachCategory}
                 >
-                  {({ selectedCategory }) => (
+                  {({ selected }) => (
                     <>
                       <span
                         className={`block truncate ${
-                          selectedCategory ? "font-medium" : "font-normal"
+                          selected ? "font-medium" : "font-normal"
                         }`}
                       >
                         {eachCategory}
                       </span>
-                      {selectedCategory ? (
+                      {selected ? (
                         <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
                           <CheckIcon className="h-5 w-5" aria-hidden="true" />
                         </span>
