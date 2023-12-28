957f0daa5d1e7429fe3c72e376e893166462f387
Ben Hessel <hesselfitness@gmail.com>
2023-12-28 17:01:31 -0500

    mobile view fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,11 +8,11 @@ interface NavbarProps {
 
 export default function Navbar({ ethPrice }: NavbarProps) {
   return (
-    <nav className="sticky top-0 z-50 px-8 h-28 max-w-[100%] flex justify-between items-center bg-slate-800">
-      <div className="relative mansalva font-display text-4xl text-transparent bg-clip-text bg-gradient-to-b from-cyan-500 to-blue-600 py-2 transform -rotate-3">
+    <nav className="sticky top-0 z-50 px-8 h-28 max-w-[100%] flex flex-col items-center justify-center sm:flex-row sm:justify-between bg-slate-800">
+      <div className="relative mansalva font-display text-4xl text-transparent bg-clip-text bg-gradient-to-b from-cyan-500 to-blue-600 py-2 sm:transform sm:-rotate-3">
         <Link href="/">DopestDAO</Link>
       </div>
-      <span className="absolute top-16 left-[184px] text-slate-100 text-base noto-sans">
+      <span className="invisible sm:visible absolute top-16 left-[184px] text-slate-100 text-base noto-sans">
         Presents...
       </span>
       <div className="flex flex-row justify-center items-center">
