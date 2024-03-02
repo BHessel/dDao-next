56a03f4844164e7116ec4a5507c1377fa66c1a65
Ben Hessel <hesselfitness@gmail.com>
2024-03-02 18:21:17 -0500

    push main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Ethereum Links - DopestDAO',
+  title: 'Dope Crypto Sites',
   description: 'A collection of Ethereum educational content',
 }
 
