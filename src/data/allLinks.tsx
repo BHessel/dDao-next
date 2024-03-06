70e634b4fb7b7864950843a13ee9520af517f29e
Mark Test <hesselb3@gmail.com>
2024-03-06 15:53:19 -0500

    mark testing

--- src/data/allLinks.tsx
+++ src/data/allLinks.tsx
@@ -1148,4 +1148,11 @@ export const allLinks: WebsiteData[] = [
       "A list of projects working on crypto data and analysis. Also has a section for AI projects.",
     category: "Lists",
   },
+  {
+    id: 155,
+    title: "testingD",
+    site: "www.google.com",
+    description: "Magic crypto site",
+    category: "Lists"
+  }
 ];
