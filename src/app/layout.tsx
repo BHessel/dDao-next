34e961bab222d725f6fe045636d7e160b49ab2dd
Ben Hessel <hesselfitness@gmail.com>
2024-03-14 19:00:26 -0400

    followed Blog version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,35 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+/* eslint-disable react/no-unescaped-entities */
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Dope Crypto Sites',
-  description: 'A collection of Ethereum educational content',
-}
+  title: "Dope Crypto Sites",
+  description: "A collection of Ethereum educational content",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', '$
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        </script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
